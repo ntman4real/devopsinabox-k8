b8cd9f113fd6a6a7ced740bc0b9b9ae3903f3fdc
Mark Friedman <mark.friedman@wipro.com>
2020-09-09 13:25:12 -0400

    Initial commit

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -1,6 +1,6 @@
 #!/usr/local/bin/bash
 echo "Building base Util container"
-docker build -t util_base ../util_containers/base_container/. &
+docker build -t util_base ./util_containers/base_container/. &
 wait
 echo "Building main Util container"
-docker build -t util_main ../util_containers/util_container/. &
+docker build -t util_main ./util_containers/util_container/. &
